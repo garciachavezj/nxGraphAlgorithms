7bebe6bf9f49f3ee910f3ddb23ce62c6ac46de4b
Jav-Zam <49886233+Jav-Zam@users.noreply.github.com>
2019-04-23 16:42:57 -0500

    Update matching.py
    I am still testing it. Feel free to work on it.

--- matching.py
+++ matching.py
@@ -1,1 +1,24 @@
+import networkx as nx
+from functions.global_properties import *
+from functions.local_properties import *
 
+G = nx.read_edgelist('graph_library/G1.txt')
+
+
+
+def is_matching(G, M): 
+    n_list = []
+    switch = True
+    G_edges = E(G)
+    
+    for e in M:
+        if e not in G_edges:
+            switch = False
+    if switch is True:
+        for outter in M:
+            temp = outter[1]
+            n_list.append(temp)
+        for outter in M:
+            temp = outter[0]
+            if temp in n_list:
+                return False
