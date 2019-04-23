3061902361c48bfc59df2720ffcda9fc69fac0ba
Jav-Zam <49886233+Jav-Zam@users.noreply.github.com>
2019-04-23 16:37:20 -0500

    Update clique.py
    Not working after further testing. I am working on it.

--- clique.py
+++ clique.py
@@ -1,1 +1,17 @@
+import networkx as nx
 
+from functions.global_properties import *
+from functions.local_properties import *
+
+G = nx.read_edgelist('graph_library/G1.txt')
+
+
+def is_clique(G, S):
+    for v in S:
+        for w in v:
+            path = neighbors(G, v)
+            if(w == v):
+                continue
+            elif(path != None and w not in path):
+                return False
+    return True
