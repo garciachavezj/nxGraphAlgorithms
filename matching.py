48f5147545426a8f29103859f39f635efa43d71c
Jav-Zam <49886233+Jav-Zam@users.noreply.github.com>
2019-04-23 23:34:36 -0500

    Tested

--- matching.py
+++ matching.py
@@ -5,20 +5,19 @@ from functions.local_properties import *
 G = nx.read_edgelist('graph_library/G1.txt')
 
 
-
-def is_matching(G, M): 
-    n_list = []
-    switch = True
-    G_edges = E(G)
-    
-    for e in M:
-        if e not in G_edges:
-            switch = False
-    if switch is True:
-        for outter in M:
-            temp = outter[1]
-            n_list.append(temp)
-        for outter in M:
-            temp = outter[0]
-            if temp in n_list:
-                return False
+def is_matching(G,S): # S param i.e. M1 = [('1', '2'), ('2', '3'), ('9', '5')] #False: is not matching
+    answer = True
+    if all(x in (E(G)) for x in S):
+        node_list = []  #collection of all nodes that appear in edge list
+        for v in S:
+            for w in v:
+                node_list.append(w)
+                total_nodes = set(node_list) # total_nodes is a set of all nodes in edge list i.e. [0,1,2]
+        for x in total_nodes:
+            if node_list.count(x) == 1:
+                continue
+            else:
+                answer = False
+    else:
+        answer = False
+    return answer
