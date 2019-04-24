8c9ef2162d4d78b5f39cd1a30017c810776959d1
Jav-Zam <49886233+Jav-Zam@users.noreply.github.com>
2019-04-24 15:38:33 -0500

    Update greedy_coloring.py

--- greedy_coloring.py
+++ greedy_coloring.py
@@ -1,18 +1,16 @@
 from functions.global_properties import *
 from functions.local_properties import *
 
-def greedy_proper_coloring(G)
+G = nx.read_edgelist('graph_library/G1.txt')
+
+def greedy_proper_coloring(G):
     colored_verticies = {v: None for v in V(G)}
-    colored_verticies[V(G)[0]] = 1
     for v in V(G):
-        if colored_verticies[v] == None:
-            N = neigbors(G, v)
-            avoid_colors = [colors[w] for w in N]
-            i = 1
-            while colored_verticies[v] == None:
-                if i in avoid_colors:
-                    i += 1
-                else:
-                    colored_verticies[v] = i
+        avoid_colors = [colored_verticies[j] for j in neighbors(G, v)]
+        i = 1
+        while colored_verticies[v] == None:
+            if i in avoid_colors:
+                i += 1
+            else:
+                colored_verticies[v] = i
     return colored_verticies
-  
