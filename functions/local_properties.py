6084c6979f172fbc6dd1f746e6a888af01942043
Jav-Zam <49886233+Jav-Zam@users.noreply.github.com>
2019-04-24 12:53:46 -0500

    Update local_properties.py

--- functions/local_properties.py
+++ functions/local_properties.py
@@ -1,1 +1,7 @@
+import networkx as nx
 
+G = nx.read_edgelist('graph_library/G1.txt')
+
+def neighbors(G,v):
+    return list(nx.neighbors(G,v)) # requires 'v'
+                                # returns each other node connected to the vert
