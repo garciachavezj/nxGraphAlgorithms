ed7e59a73d12f8ab2bfa42d2ad7623aadc14c027
Ibrahim-Islam11 <49886113+Ibrahim-Islam11@users.noreply.github.com>
2019-04-23 21:01:55 -0500

    Update havelhakimi.py

--- havelhakimi.py
+++ havelhakimi.py
@@ -1,1 +1,22 @@
+L = [4, 3, 3, 2, 2, 2]
 
+def Havel_Hakimi_derivative(L):
+  d_1 = L[0]
+  L.pop(0)
+  for i in range(d_1):
+    L[i] -= 1
+    L.sort(reverse = True)
+    return None
+  
+def Havel_Hakimi_Process(L, show = True):
+  if show == True:
+    print(L)
+  while L[0] > 0:
+    Havel_Hakimi_derivative(L)
+    if show == True:
+      print(L)
+    return None
+ 
+def is_graphic(L):
+  Havel_Hakimi_Process(L)
+  return sum(L) == 0
