5211a8e405535a5ebd0da98bb67074ade101fce1
martindao <35680899+martindao@users.noreply.github.com>
2019-04-24 00:05:04 -0500

    Add files via upload

---
+++ triangle_free.py
@@ -0,0 +1,18 @@
+
+print('The graph vertices are {}'.format(V(G)))
+print('The graph edges are {}'.format(E(G)))
+
+def triangle_free(G):
+    for edge in E(G):
+        u,v=edge
+        for vertex in V(G):
+            
+            if vertex in edge:
+                continue
+            if (u,vertex) in E(G) or (vertex,u) in E(G):
+                if (v,vertex) in E(G) or (vertex,v) in E(G):
+                    print('Triangle found with vertices {} {} {}'.format(u,v,vertex))
+                    return False
+    return True
+
+print('Triangle free? {}'.format(triangle_free(G)))
