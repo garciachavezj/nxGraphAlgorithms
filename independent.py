b9d2512f3b0a4e6305d0d0b01e59b63c4c7f1486
martindao <35680899+martindao@users.noreply.github.com>
2019-04-24 13:53:36 -0500

    Update independent.py

--- independent.py
+++ independent.py
@@ -1,6 +1,17 @@
+import networkx as nx
+from intertools import combinations
+from functions.global_properties import V
+from functions.local_properties import neighbors
+
 def is_independent(G, S):
-  for v in S:
-    if list(set(S) & set(neighbors(G, v))) != []:
+  for V in S:
+    if list(set(S) & set(neighbors(G,V))) != []:
       return False
   return True
   
+def maximum_independent_set(G):
+n = len(V(G))
+for k in range(n-1,1,-1):
+    for S in combinations(V(G),t);:
+        if is_independent (6, list(3))==True:
+            return list(S)
