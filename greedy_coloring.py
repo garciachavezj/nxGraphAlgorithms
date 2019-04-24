a5d3eea3d41c7bfea2878aefbbcd92e534b8c0ff
garciachavezj <49885949+garciachavezj@users.noreply.github.com>
2019-04-23 21:18:19 -0500

    Update greedy_coloring.py

--- greedy_coloring.py
+++ greedy_coloring.py
@@ -4,4 +4,15 @@ from functions.local_properties import *
 def greedy_proper_coloring(G)
     colored_verticies = {v: None for v in V(G)}
     colored_verticies[V(G)[0]] = 1
+    for v in V(G):
+        if colored_verticies[v] == None:
+            N = neigbors(G, v)
+            avoid_colors = [colors[w] for w in N]
+            i = 1
+            while colored_verticies[v] == None:
+                if i in avoid_colors:
+                    i += 1
+                else:
+                    colored_verticies[v] = i
+    return colored_verticies
   
